1e35f90205f12a92623ba5d7c5c78167290d7a7d
JaehyeokSong0 <jaehyeok.song0@gmail.com>
2021-02-03 22:04:59 +0900

    5924KB / 4ms

--- 1000~1999/1463.cpp
+++ 1000~1999/1463.cpp
@@ -3,27 +3,24 @@
 using namespace std;
 
 #define MAXSIZE 1000000+1
-int memo[MAXSIZE]; //calc(n)의 값 memoization
-
-int calc(int n)
-{
-	if (n == 1)
-		return 0;
-	else if (memo[n] > 0) //이미 구한 calc(n)값이면 바로 값 반환
-		return memo[n];
-	memo[n] = calc(n - 1) + 1; //calc(n)의 초깃값 (아래 과정을 수행하면서 더 작은 값 발견 시 교체)
-	if (n % 2 == 0)
-		memo[n] = min(memo[n], calc(n / 2) + 1); //현재까지 저장된 calc(n)값보다 calc(n/2)+1이 더 작으면 memo[n]값 교체
-	if (n % 3 == 0)
-		memo[n] = min(memo[n], calc(n / 3) + 1);
-	return memo[n];
-}
+int memo[MAXSIZE] = { 0, }; //연산 횟수의 최솟값 memoization
 
 int main()
 {
 	int n;
 	cin >> n;
+	memo[2] = memo[3] = 1;
 
-	cout << calc(n);
+	for (int i = 4;i <= n;i++)
+	{
+		memo[i] = memo[i - 1] + 1; //memo[i]의 초깃값 (반복문 수행하면서 더 작은 값 발견 시 교체)
+		if (i % 2 == 0)
+			memo[i] = min(memo[i], memo[i / 2] + 1);
+		if (i % 3 == 0)
+			memo[i] = min(memo[i], memo[i / 3] + 1);
+	}
+
+	cout << memo[n];
+
 	return 0;
 }
