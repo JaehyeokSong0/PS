343d67f1308197db8c7b1d761469693e6542f201
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2021-07-07 23:13:24 +0900

    2020KB / 0ms

---
+++ 10000~10999/10171.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	
+	cout << "\\    /\\"<<endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+
+	return 0;
+}
