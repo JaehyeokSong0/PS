a16e966696218e10bdd5dcb2a5bdcbe117bfbaee
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2021-07-16 23:34:40 +0900

    2020KB / 4ms

---
+++ 10000~10999/10950.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t, a, b;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+
+	return 0;
+}
