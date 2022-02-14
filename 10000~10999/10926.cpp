fd70c69bd96f7293a0285d6ef6df1cdedd53f6b2
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2022-02-14 22:21:07 +0900

    2024KB / 0ms

---
+++ 10000~10999/10926.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+    cout << s <<"??!";
+    return 0;
+}
