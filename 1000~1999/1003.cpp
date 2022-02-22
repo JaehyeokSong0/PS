dd3a52b32b26166c6a78d9b371a105f539dd3781
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2022-02-22 21:27:47 +0900

    2020KB / 0ms

---
+++ 1000~1999/1003.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+#define MAXSIZE 41
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int memo[MAXSIZE] = { 0,1 };
+	int t,n;
+	cin >> t;
+
+	for (int i = 2; i < MAXSIZE; i++)
+		memo[i] = memo[i - 2] + memo[i - 1];
+
+	while (t--)
+	{
+		cin >> n;
+		if (n)
+			cout << memo[n - 1] << " " << memo[n] << "\n";
+		else
+			cout << "1 0\n";
+	}
+}
